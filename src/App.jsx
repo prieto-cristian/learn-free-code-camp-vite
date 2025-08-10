b738fdc17ae3c8bd4c17d663a278eef353ece91a
Prieto Cristian <pcristiandev@gmail.com>
2025-08-10 12:46:04 -0300

    Subir el proyecto a GitHub

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Title = () =>{
+  return <div>
+    hello world!
+  </div>
+}
+
+const App = () => {
+  return <div>
+    todo
+  </div>;
+};
+
+export default App
